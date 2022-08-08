b70c3fddb0c4176b78f0aa3250481bd78abadc22
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-08-08 14:46:20 -0400

    adding into html,css and script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "TRAVEL AND SEE THE BEAUTY OF OUR MOTHERLAND";
 document.querySelector("body").appendChild(h2);
+let slideIndex = 0;
+showSlides();
+
+function showSlides(){
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i=0;i<slides.length;i++){
+        slides[i].style.display = "none";
+    }
+  slidesIndex++;
+  if(slideIndex>slides.length) {slideIndex = 1}
+  slides[slidesIndex-1].style.display = "block";
+  setTimeout(showSlides,2000);
+  }  
