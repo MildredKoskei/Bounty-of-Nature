93ea43804cc8ca8f4af66315d36874b93959a3ab
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-08-13 13:05:29 -0400

    adding into html,css and javaScript

--- script.js
+++ script.js
@@ -1,17 +1,63 @@
-const h2 = document.createElement("h2");
+/*const h2 = document.createElement("h2");
 h2.textContent = "TRAVEL AND SEE THE BEAUTY OF OUR MOTHERLAND";
-document.querySelector("body").appendChild(h2);
-let slideIndex = 0;
-showSlides();
+document.querySelector("body").appendChild(h2);*/
 
+var slideIndex,slides,dots,captionText; 
 function showSlides(){
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    for (i=0;i<slides.length;i++){
-        slides[i].style.display = "none";
+    slideIndex = 0;
+    slides=document.getElementsByClassName("imageHolder");
+    slides[slideIndex].style.opacity = 1;
+
+    captionText= document.querySelector(".captionHolder .captionText");
+    captionText.innerText=slides[slideIndex].querySelector(".captionText").innerText;
+
+ dots=[];
+ var dotsContainer=document.getElementById("dotsContainer");
+   for (var i=0;i<slides.length;i++){
+    var dot = document.createElement("span");
+    dot.classList.add("dots");
+    dotsContainer.append(dot);
+    dots.push(dot);
+
+   dots[slideIndex].classList.add("active");
+
+   }
+
+
+
+
+
+}
+showSlides(); 
+
+
+/*let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+
+
+//function showSlides(n) {
+    //let i;
+    //let slides = document.getElementsByClassName("mySlides");
+    //let dots = document.getElementsByClassName("demo");
+    /*let captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
     }
-  slidesIndex++;
-  if(slideIndex>slides.length) {slideIndex = 1}
-  slides[slidesIndex-1].style.display = "block";
-  setTimeout(showSlides,2000);
-  }  
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  
+  */
